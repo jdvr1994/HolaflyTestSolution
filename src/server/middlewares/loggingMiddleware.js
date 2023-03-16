72e5e8e5ff4f90dcc40fb28239a02dab807797b5
Juan Velasquez <33909059+jdvr1994@users.noreply.github.com>
2023-03-16 05:28:40 -0500

    feat (logging): added logger functionality to middleware

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -1,9 +1,14 @@
-const loggingMiddleware = (db) =>
-    (req, res, next) => {
+const loggingMiddleware = (app) =>
+    async (req, res, next) => {
         const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
         const headers = JSON.stringify(req.headers);
         const originalUrl = req.originalUrl;
         // Persist this info on DB
+        const data = await app.controllers.logController.createEntity({
+            action: originalUrl,
+            header: headers,
+            ip: ip
+        });
         next();
     }
 
