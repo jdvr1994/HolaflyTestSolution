7b0b4e94c86b6a5579b34c4110dd3db655b58343
Juan Velasquez <33909059+jdvr1994@users.noreply.github.com>
2023-03-16 05:34:52 -0500

    feat (getLogs): added logController to app

--- src/app/index.js
+++ src/app/index.js
@@ -5,12 +5,14 @@ const swapiFunctions = require('./swapiFunctions')
 
 const peopleSeqRepository = new repositories.peopleSequalize(db)
 const planetSeqRepository = new repositories.planetSequalize(db)
+const logSeqRepository = new repositories.logSequalize(db)
 const peopleSwapiRepository = new repositories.peopleSwapiAPI(swapiFunctions)
 const planetSwapiRepository = new repositories.planetSwapiAPI(swapiFunctions)
 
 const controllers = {
     peopleController: new appControllers.people(peopleSeqRepository,peopleSwapiRepository,planetSeqRepository,planetSwapiRepository),
     planetController: new appControllers.planet(planetSeqRepository,planetSwapiRepository),
+    logController: new appControllers.log(logSeqRepository,null)
 }
 
 module.exports = {
